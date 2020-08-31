cbe1d3358f061d44a9f6b62ccb2d507068426b15
MOHD ARAFAT <42473987+mohdarafat@users.noreply.github.com>
2020-09-01 03:09:45 +0530

    Add files via upload

--- formsubmit.php
+++ formsubmit.php
@@ -1,21 +1,8 @@
 <?php
-include "db.php";
-$name=$_POST['NAME'];
-$contact = $_POST['CONTACT']; 
-$email = $_POST['EMAIL']; 
-$city = $_POST['CITY']; 
-$designation = $_POST['DESIGNATION']; 
-$coursename = $_POST['COURSE']; 
 
-try {
-    $stmt = $conn->prepare("INSERT INTO userenquiry(Name, Email, Phone, City, Designation, Course) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssisss", $name, $email, $contact, $city, $designation, $coursename);
-    $stmt->execute();
-    $stmt->close();
-    echo "Success";
-}
-catch(Exception $e){
-    echo 'Fail:'.$e->getMessage(); 
-}
+$name=$_POST['name'];
+
+
+
 
 ?>
